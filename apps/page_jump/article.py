926aa4cf776c9fffa36fd4899ce8d2557731aefa
1CanzhenXiao <842462376@qq.com>
2022-08-24 00:32:19 +0800

    update

--- apps/page_jump/article.py
+++ apps/page_jump/article.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, render_template
+import json
 
 bp = Blueprint("article",__name__)
 
 @bp.route('/<id>')
 def article(id):
-    return render_template("article.html")
+    with open(r"D:\git\juejin\static\resource\json\all_user_info.json", 'r', encoding='utf-8') as f:
+        users_inf = json.load(f)
+    user_inf = users_inf[id]
+    return render_template("article.html", user_inf = user_inf, method = "post")
