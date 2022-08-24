a1f186eb91c318aaa27bbfad3bc1a5d5b389cabf
1CanzhenXiao <842462376@qq.com>
2022-08-24 17:58:07 +0800

    update

--- apps/page_jump/article.py
+++ apps/page_jump/article.py
@@ -3,9 +3,14 @@ import json
 
 bp = Blueprint("article",__name__)
 
-@bp.route('/<id>')
-def article(id):
+@bp.route('/<article_id>')
+def article(article_id):
+    with open(r"D:\git\juejin\static\resource\json\all_article_info.json", 'r', encoding='utf-8') as f:
+        articles_inf = json.load(f)
     with open(r"D:\git\juejin\static\resource\json\all_user_info.json", 'r', encoding='utf-8') as f:
         users_inf = json.load(f)
-    user_inf = users_inf[id]
-    return render_template("article.html", user_inf = user_inf, method = "post")
+
+    article_inf = articles_inf[article_id]
+    user_id = article_inf["user_id"]
+    user_inf = users_inf[user_id]
+    return render_template("article.html", user_inf = user_inf, article_inf = article_inf, method = "post")
